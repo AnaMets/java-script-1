a78a4a51b911a71cc81684a1bf0ce818f443be69
Anastasiia Mets <anastasiia.mets@gmail.com>
2018-04-30 16:01:37 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,40 @@
+/*alert('Hello');*/
+//document.write('<h1>World</h1>');
+//console.log('Hello World');
+
+// const carName = 'Tesla Model X';
+// const purchaseCarPrice = 100000;
+// const isAvailable = true;
+
+// document.write(carName);
+// document.write('<br>');
+// document.write(purchaseCarPrice);
+// document.write('<br>');
+// document.write(isAvailable);
+
+
+// const saleCarPrice = purchaseCarPrice * 1.1;
+
+// const age = parseInt(prompt('Enter your age'));
+// const newAge = age + 1;
+// document.write('Next year you will be ' + newAge);
+
+
+// const age = parseInt(prompt('Enter your age'));
+// if ( (age >= 18) && (age < 70) ) {
+//     document.write('You are adult');
+// } else if (age < 18) {
+//     document.write('You are child');
+// } else {
+//     document.write('You are old');
+// }
+
+function add(a,b,c) {
+    const result = a+b+c;
+    return result;
+}
+
+const sum = add(100,200,300);
+document.write(sum);
+
+document.write(Math.sqrt(16));
